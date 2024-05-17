6fa2476789e07150963eb21413e7bf55fc4ea1dc
mcz9mm <k.m.swim411@gmail.com>
2024-05-17 10:03:12 +0900

    Initial Commit

---
+++ TooDooApp/TooDooAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  TooDooAppApp.swift
+//  TooDooApp
+//
+//  Created by kaoru matarai on 2024/05/17.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TooDooAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
